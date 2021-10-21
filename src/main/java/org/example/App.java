655f638d5114422309a5d41e4d1e1d2bd2745e91
sahanaka <weraniyagoda97@gmail.com>
2021-10-21 19:47:41 +0530

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Shape circle = new Circle();
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle with red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle with red border");
+        redRectangle.draw();
+    }
+}
---
+++ src/main/java/org/example/ShapeDecorator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+    }
+}
